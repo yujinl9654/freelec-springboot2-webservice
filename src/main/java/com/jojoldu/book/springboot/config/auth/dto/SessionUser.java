608fcc66b66d20d39c46150eedd4a955fde5c4ba
Yujin Lee <yujinl@gmail.com>
2021-03-21 21:49:05 +0900

    google Login

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.jojoldu.book.springboot.config.auth.dto;
+
+import com.jojoldu.book.springboot.domain.user.User;
+import java.io.Serializable;
+import lombok.Getter;
+
+@Getter
+public class SessionUser implements Serializable {
+  private String name;
+  private String email;
+  private String picture;
+
+  public SessionUser(User user) {
+    this.name = user.getName();
+    this.email = user.getEmail();
+    this.picture = user.getPicture();
+  }
+
+}
--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,7 +1,9 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +15,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
   private final PostsService postsService;
+  private final HttpSession httpSession;
 
   @GetMapping("/")
   public String index(Model model) {
     model.addAttribute("posts", postsService.findAllDesc());
+    SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+    if (user != null) {
+      model.addAttribute("userName", user.getName());
+    }
     return "index";
   }
 
